3d660366a7053d2fd60a9aa7dcf29707305f8639
Gilberto <gilbertowuwu0@gmail.com>
2024-11-14 18:47:04 -0500

    header added

---
+++ components/header.js
@@ -0,0 +1,71 @@
+import { View, Text, Image, FlatList, Pressable, StyleSheet } from "react-native";
+
+export default function Header({}){
+  const paginas = ['home', 'agregar pagina'];
+
+  return (
+    <View style={styles.container}>
+      {/*left logo*/} 
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>MIS PAGINAS</Text>
+      </View> 
+
+      {/*Links*/} 
+      <FlatList
+        contentContainerStyle={styles.linkContainer}
+        data={paginas}
+        horizontal={true}
+        renderItem={({item, index})=> <Pressable style={styles.link} key={`pagina-${item}`}><Text style={styles.subTitle}>{item}</Text></Pressable>}
+      />
+
+      {/*User details*/} 
+      <View style={styles.userDetailContainer}>
+        <View>
+          <Text style={styles.subTitle}>Orange Gat</Text>
+          <Text>Ver usuario</Text>
+        </View>
+        <View><Pressable><Image style={styles.pfp} source={require("../assets/adaptive-icon.png")} /></Pressable></View>
+      </View> 
+    </View>
+  )
+}
+
+const styles = {
+  container: {
+    height: "100px",
+    width: "100%",
+    overflow: "hidden",
+    flexDirection: "row",
+    paddingHorizontal: "50px",
+    paddingTop: "-10px",
+    alignItems: "center",
+  },
+  titleContainer: {
+    flexBasis: "300px",
+  },
+  subTitle: {
+    fontWeight: "800"
+  },
+  title: {
+    fontSize: "2em",
+    fontWeight: "800"
+  },
+  pfp: {
+    height: "50px",
+    width: "50px",
+    borderWidth: "1px",
+    borderRadius: "30px",
+  },
+  linkContainer: {
+    flex: 1,
+    gap: "100px",
+  },
+  link: {
+
+  },
+  userDetailContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: "10px",
+  }
+}
---
+++ screens/home.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import Header from "../components/header";
+
+export default function Home({}){
+  return (
+    <View>
+      <Header />
+    </View>
+  )
+}
